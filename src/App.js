092a30b71acb513b2e4528904d8e04cd8957ead0
EC2 Default User <ec2-user@ip-172-31-24-221.ap-northeast-1.compute.internal>
2020-12-21 14:51:32 +0000

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React, { Component } from 'react';
 
-function App() {
-  return (<div>Hello World!</div>);
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type='text' onClick={() => {console.log("test")}} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
